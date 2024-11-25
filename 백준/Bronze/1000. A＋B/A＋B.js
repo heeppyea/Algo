73301c0b66b2d727fe661c47b3cd7ae4e4db877e
heeppyea <111338578+heeppyea@users.noreply.github.com>
2024-11-25 22:24:09 +1000

    [Bronze V] Title: A+B, Time: 96 ms, Memory: 9344 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.js
+++ 백준/Bronze/1000. A＋B/A＋B.js
@@ -1,7 +1,7 @@
-let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().split('\n');
-let line = input[0].split(' ');
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
-let a = parseInt(line[0]); 
+let line = input[0].split(" ");
+let a = parseInt(line[0]);
 let b = parseInt(line[1]);
-console.log(a + b)
+console.log(a + b);
