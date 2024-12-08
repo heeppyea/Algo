c6144437f7543b4164164fd6f69df369afa95a2d
heeppyea <111338578+heeppyea@users.noreply.github.com>
2024-12-08 10:45:14 +0900

    [Bronze V] Title: A+B - 5, Time: 100 ms, Memory: 9560 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.js
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.js
@@ -1,13 +1,15 @@
 let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().split('\n')
 
-for(let i=0;i<input.length;i++){
-    let A = parseInt(input[i].split(' ')[0]);
-    let B = parseInt(input[i].split(' ')[1]);
-    let result = A + B;
-  
-    if(result===0){
-        break;
-    } else {console.log(result)
-        };
-};
+input.forEach((v) => {
+    // 입력이 유효한지 확인
+    if (!v) return; // 빈 줄이나 undefined를 무시
+
+    let parts = v.split(' ');
+    let num1 = parseInt(parts[0]);
+    let num2 = parseInt(parts[1]);
+    let result = num1 + num2;
+
+    if (result === 0) return; // 결과가 0이면 무시
+    console.log(result);
+});
