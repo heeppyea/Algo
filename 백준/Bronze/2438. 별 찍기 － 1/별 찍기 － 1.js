404eedf08918683d2f7d6dd7ac70e1c6446a47a0
heeppyea <111338578+heeppyea@users.noreply.github.com>
2024-12-08 09:44:17 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 96 ms, Memory: 9484 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.js
@@ -0,0 +1,16 @@
+
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split(' ');
+
+let num = Number(input);
+
+result = '';
+
+for (let i = 0; i < num; i++) {
+  for (let j = 0; j <= i; j++) {
+    result += '*';
+  }
+  result += '\n';
+}
+
+console.log(result);
