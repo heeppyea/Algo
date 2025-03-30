3051c3178bcd0ce1239fb0b6777539608c4d5277
heeppyea <111338578+heeppyea@users.noreply.github.com>
2025-03-30 17:09:58 +0900

    [Bronze II] Title: 나머지, Time: 92 ms, Memory: 9332 KB -BaekjoonHub

--- node.js/백준/Bronze/3052. 나머지/나머지.js
+++ node.js/백준/Bronze/3052. 나머지/나머지.js
@@ -1,13 +1,10 @@
 let fs = require('fs');
 let input = fs.readFileSync("/dev/stdin").toString().split("\n");
-
 const arr = input.slice(0,10).map((v) => Number(v));
-const restArr = new Set();
-
+const result = new Set();
 for (let i = 0; i < arr.length; i ++) {
     const rest = arr[i] % 42;
-    restArr.add(rest);
+    result.add(rest);
 }
 
-const result = restArr.size;
-console.log(result);
+console.log(result.size);
