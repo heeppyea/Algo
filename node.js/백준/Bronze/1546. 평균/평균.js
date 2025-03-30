fcf15c6a6c75d10c05f7fcc5ec2d73f46ad6afed
heeppyea <111338578+heeppyea@users.noreply.github.com>
2025-03-30 18:14:14 +0900

    [Bronze I] Title: 평균, Time: 96 ms, Memory: 9340 KB -BaekjoonHub

---
+++ node.js/백준/Bronze/1546. 평균/평균.js
@@ -0,0 +1,14 @@
+let fs = require('fs');
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const num = Number(input[0]);
+const scores = input[1].split(' ').map(Number);
+const maxValue = Math.max(...scores);
+
+const result = scores.reduce((acc, cur, i)  => {
+    acc += cur / maxValue * 100;
+    return acc;
+}, 0)
+
+const average = (result / num).toFixed(3);
+console.log(average);
