fd03fcd68de27f872e1a1ebb2f701cf867e17823
heeppyea <111338578+heeppyea@users.noreply.github.com>
2025-03-30 18:02:44 +0900

    [Bronze III] Title: 최소, 최대, Time: 388 ms, Memory: 95180 KB -BaekjoonHub

--- node.js/백준/Bronze/10818. 최소， 최대/최소， 최대.js
+++ node.js/백준/Bronze/10818. 최소， 최대/최소， 최대.js
@@ -1,16 +1,10 @@
 let fs = require('fs');
-let input = fs.readFileSync("/dev/stdin").toString().split('\n');
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
-// 값 초기화
-let n = Number(input[0]);
-let arr = input[1].split(' ').map(Number);
+const arr = input[1].split(' ').map(Number);
 
-let minValue = 1000001; // 일단 큰 수로 초기화
-let maxValue = -1000001; // 일단 작은 수로 초기화 
 
-for (let i = 0; i < n; i++) {
-    if(minValue > arr[i]) minValue = arr[i];
-    if(maxValue < arr[i]) maxValue = arr[i];
-}
+const max = Math.max(...arr);
+const min = Math.min(...arr);
 
-console.log(minValue, maxValue);
+console.log(min, max)
