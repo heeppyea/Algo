8e0714449141ce3a7f06857a49d2e64266301668
heeppyea <111338578+heeppyea@users.noreply.github.com>
2025-03-30 19:44:22 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 96 ms, Memory: 9408 KB -BaekjoonHub

--- node.js/백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.js
+++ node.js/백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.js
@@ -18,3 +18,30 @@ for (let i = 1; i <= num; i ++) {
 
 }
 console.log(result);
+
+// 강의 풀이
+// function check(data) {
+//     let setData = new Set(data[0]);
+//     for (let i = 0; i < data.length -1; i++) {
+//         // 오른쪽 단어와 다르다면
+//         if(data[i] !== data[i +1]) {
+//             // 이미 등장한 적 있는 알파벳이라면
+//             if(setData.has(data[i + 1])) {
+//                 return false;
+//             }
+//             else {
+//                 setData.add(data[i + 1]);
+//             }
+//         }
+//     }
+//     return true;
+// }
+//
+// let n = Number(input[0]);
+// let summary = 0;
+//
+// for (let i = 1; i <= num; i++) {
+//     let data = input[i];
+//     if(check(data)) summary +=1;
+// }
+// console.log(summary);
