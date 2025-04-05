471aae33daba6518e5913ee0d9d0ce68725b90b0
heeppyea <111338578+heeppyea@users.noreply.github.com>
2025-04-05 20:19:42 +0900

    Update 좌표 정렬하기.js

--- node.js/백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.js
+++ node.js/백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.js
@@ -15,3 +15,16 @@ const sorted = coords.sort((a,b) => {
 for (const v of sorted) {
     console.log(v[0], v[1]);
 }
+
+// 단순화 
+// const coords = input.slice(1).map(line => line.split(' ').map(Number));
+//
+// const sorted = coords.sort((a,b) => {
+//     if(a[0] !== b[0]) return a[0] - b[0]
+//     else return a[1] - b[1]
+// })
+//
+// for (const v of sorted) {
+//     console.log(v[0], v[1]);
+// }
+
