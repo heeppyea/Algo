9bc2fb24bd66fae28605727a04dbd3c24c441d5a
heeppyea <111338578+heeppyea@users.noreply.github.com>
2025-04-05 19:17:59 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1232 ms, Memory: 217876 KB -BaekjoonHub

--- node.js/백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.js
+++ node.js/백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.js
@@ -2,38 +2,5 @@ let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(Number);
 
 const arr = input.slice(1);
-
-function merge(arr) {
-    if(arr.length < 2) {
-        return arr;
-    }
-
-    const mid = Math.floor(arr.length / 2);
-    const left = arr.slice(0, mid);
-    const right = arr.slice(mid);
-
-    return mergeSort(merge(left), merge(right));
-
-    function mergeSort(left, right) {
-        const mergedArr = [];
-        let leftIdx = 0;
-        let rightIdx = 0;
-
-        while(leftIdx < left.length && rightIdx < right.length) {
-            if(left[leftIdx] < right[rightIdx]) {
-                mergedArr.push(left[leftIdx]);
-                leftIdx ++;
-            } else {
-                mergedArr.push(right[rightIdx]);
-                rightIdx++;
-            }
-        }
-
-        return mergedArr.concat(left.slice(leftIdx), right.slice(rightIdx));
-
-    }
-
-}
-
-const result = merge(arr).join('\n');
-console.log(result)
+const result = arr.sort((a,b) => a -b).join('\n');
+console.log(result);
