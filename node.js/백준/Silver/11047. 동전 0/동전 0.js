3cb3e8c27f4e43045d742b774bc896eaaad744f3
heeppyea <111338578+heeppyea@users.noreply.github.com>
2025-04-06 14:45:59 +0900

    [Silver IV] Title: 동전 0, Time: 92 ms, Memory: 9600 KB -BaekjoonHub

---
+++ node.js/백준/Silver/11047. 동전 0/동전 0.js
@@ -0,0 +1,19 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const money = Number(input[0].split(' ')[1])
+const coins = input.slice(1).map(Number)
+const sortedCoin = coins.sort((a, b) => b - a);
+
+let num = 0;
+let calcMoney = money
+
+for (const v of sortedCoin) {
+    if (calcMoney > 0 && calcMoney >= v) {
+        const share = Math.floor(calcMoney / v) // 나머지 제외 몫만
+        num += share
+        calcMoney -= v * share;
+    }
+}
+
+console.log(num)
