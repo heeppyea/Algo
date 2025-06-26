e84a3a8e77d7b2799ecd2a467c00dfe693a94387
hetzzu <qkr04230713@gmail.com>
2025-06-26 23:05:09 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 112 ms, Memory: 9384 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.js
@@ -0,0 +1,38 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().trim();
+
+const obj = {
+    'a': 0,
+    'b': 0,
+    'c': 0,
+    'd': 0,
+    'e': 0,
+    'f': 0,
+    'g': 0,
+    'h': 0,
+    'i': 0,
+    'j': 0,
+    'k': 0,
+    'l': 0,
+    'm': 0,
+    'n': 0,
+    'o': 0,
+    'p': 0,
+    'q': 0,
+    'r': 0,
+    's': 0,
+    't': 0,
+    'u': 0,
+    'v': 0,
+    'w': 0,
+    'x': 0,
+    'y': 0,
+    'z': 0
+};
+
+input.split('').forEach((v) => {
+    obj[v] += 1;
+})
+
+const result = Object.values(obj).map((v) => v);
+console.log(...result)
